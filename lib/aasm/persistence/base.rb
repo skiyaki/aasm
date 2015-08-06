30c1df54c6174d8a6bf60d1ce4abcd4341ae3aff
yui-knk <spiketeika@gmail.com>
2015-08-06 16:37:44 +0900

    Remove @klass.table_name from scope conditions key

--- lib/aasm/persistence/base.rb
+++ lib/aasm/persistence/base.rb
@@ -59,6 +59,7 @@ module AASM
         if @klass.ancestors.map {|klass| klass.to_s}.include?("ActiveRecord::Base")
 
           conditions = {"#{@klass.table_name}.#{@klass.aasm.attribute_name}" => name.to_s}
+          conditions = {"#{@klass.aasm.attribute_name}" => name.to_s}
           if ActiveRecord::VERSION::MAJOR >= 3
             @klass.class_eval do
               scope name, lambda { where(conditions) }
